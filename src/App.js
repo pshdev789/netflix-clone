c992c874a67ba60ef2b97a727574a842d786ae5f
pshdev789 <kanchikakodikara@gmail.com>
2020-10-31 12:20:07 +0530

    Added Basic styling

--- src/App.js
+++ src/App.js
@@ -2,18 +2,27 @@
 //  U write a Componenet and can reuse it using props-properties
 import "./App.css";
 import Row from "./Row";
-import requests from './request';
+import requests from "./request";
+import Banner from "./Banner";
 
 function App() {
   //useState is a react hook
 
-
-
   return (
-    <div className="App">
-      <h1>Hay Programmer Let's Build Netflix clone FrontEnd 🚀  </h1>
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+    <div className="app">
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,25 +1,41 @@
 import React, { useState, useEffect } from "react";
-import axios from './axios';
+import axios from "./axios";
+import "./Row.css";
 
-function Row({ title, fetchUrl }) {
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
 
-//   When the Row loads need to pull the info from the tmdb
-// basically when a row loads this snippet of code runs
-// since the [] is empty (they call it a dependency), it will invoke only ones
-// otherwise it will invoke each time the proerty value changes
+  //   When the Row loads need to pull the info from the tmdb
+  // basically when a row loads this snippet of code runs
+  // since the [] is empty (they call it a dependency), it will invoke only ones
+  // otherwise it will invoke each time the proerty value changes
   useEffect(() => {
-
-    async function fetchData(){
-        const request = await axios.get(fetchUrl);
-        console.log(request);
-        return request;
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
     }
     fetchData();
-  }, [])
+  }, [fetchUrl]);
+
+  console.log(movies);
   return (
-    <div>
+    <div className="row">
       <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
     </div>
   );
 }
